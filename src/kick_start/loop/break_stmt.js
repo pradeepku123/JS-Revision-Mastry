0283eaafc88018dedcd298ed4aa3b79702c33b9f
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-01-03 19:54:13 +0530

    break stmt

---
+++ src/kick_start/loop/break_stmt.js
@@ -0,0 +1,12 @@
+/**
+ * JS Break Statment
+ * Use break statment to Terminate  a loop,Switch Statment or Cojuction
+ * Labeled Statements
+ */
+
+// For Loop With Break Statment
+
+for (let i = 0; i < 5; i++) {
+  if (i === "3") break;
+  console.log(`i = ${i}`);
+}
