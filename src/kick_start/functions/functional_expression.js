4fc99519ca9b32bb716424c50eea0541f17fb19c
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-01-22 10:56:47 +0530

    Functional Expression

---
+++ src/kick_start/functions/functional_expression.js
@@ -0,0 +1,18 @@
+/**
+ * Functional Expressions
+ */
+
+console.log(`Welcome of Funcional Expressions`);
+
+const square = function (ele) {
+  return ele * ele;
+};
+
+/**
+ * Arrow Functions
+ */
+const arrowNum = (num) => num * num;
+
+console.log(square(8));
+
+console.log(arrowNum(3));
