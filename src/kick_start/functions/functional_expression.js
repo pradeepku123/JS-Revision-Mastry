81b86e921e53d87638966214ba8fc5b374aad238
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-01-22 11:03:49 +0530

    Factorial of Number

--- src/kick_start/functions/functional_expression.js
+++ src/kick_start/functions/functional_expression.js
@@ -16,3 +16,15 @@ const arrowNum = (num) => num * num;
 console.log(square(8));
 
 console.log(arrowNum(3));
+
+//Find a Factorial of Number
+
+function findFact(num) {
+  if (num <= 1) {
+    return 1;
+  } else if (num > 1) {
+    return num * findFact(--num);
+  }
+}
+
+console.log(findFact(3));
