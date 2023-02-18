3392f66bba324600289517622564c0e9468b2135
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-02-18 09:37:03 +0530

    expl date object

---
+++ src/kick_start/numbers_and_dates/date_obj.js
@@ -0,0 +1,14 @@
+/**
+ * Date Objects
+ */
+
+console.log(`Date Objects Exp`);
+
+const myDate = new Date();
+
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getTime());
+console.log(myDate.getHours());
+
+console.log(myDate);
