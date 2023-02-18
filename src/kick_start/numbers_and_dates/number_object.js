611acc33f3c3721832a0010953e9e996140ade3a
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-02-18 07:36:44 +0530

    Number object

---
+++ src/kick_start/numbers_and_dates/number_object.js
@@ -0,0 +1,11 @@
+/**
+ * Number Object
+ */
+
+console.log(`Welcome To Number Object`);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.NaN);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
