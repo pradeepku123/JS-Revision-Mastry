0daaaed32ad26426a1ae0b81555155839405c4cb
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-02-18 07:11:20 +0530

    add numbers

---
+++ src/kick_start/numbers_and_dates/number.js
@@ -0,0 +1,28 @@
+/**
+ * Numbers And Dates
+ */
+
+console.log(`Welcome to Numbers & Dates Exploration`);
+
+//Numbers
+
+const decNumber = 99;
+console.log(decNumber);
+console.log(typeof decNumber);
+
+// binary
+
+const binNumber = 0b10101;
+console.log(binNumber);
+console.log(typeof binNumber);
+
+// Octal
+
+const octalNumber = 0o234;
+const octalNumber2 = 0o123;
+console.log(octalNumber, octalNumber2);
+
+//HexaDecimal
+
+const hexaDecimal = 0xface;
+console.log(hexaDecimal);
