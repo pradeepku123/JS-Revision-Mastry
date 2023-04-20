18a6da07838365b92a7d98c0f8eb22b9a3a341d0
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-04-20 07:23:59 +0530

    Obj Prop

---
+++ src/kick_start/objects_exp/object_exp.js
@@ -0,0 +1,37 @@
+/**
+ * Explore Object Creation & Diffrent Methods
+ */
+
+console.log(`Welcome to Object`);
+
+//Creare Object using Object.create() method
+
+// Structure of Object
+
+const animal = {
+  type: "NewAnimal-001",
+  displayType() {
+    return this.type;
+  },
+};
+
+const find_animal = Object.create(animal);
+
+console.log(find_animal.displayType());
+
+find_animal.type = "Changed Animal";
+console.log(find_animal.displayType());
+
+console.log(animal.displayType());
+
+const myCarProperty = {
+  name: "Toyota",
+  place: "BAM",
+  size: "23",
+};
+
+Object.keys(myCarProperty).forEach((i) => {
+  console.log(myCarProperty[i]);
+});
+
+console.log(Object.entries(myCarProperty));
