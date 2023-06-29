69837cfe1114cb94792d11e46d88001db28322d4
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-06-29 18:49:10 +0530

    JS_1rray_1.4

--- src/kick_start/array/arr_2nd.js
+++ src/kick_start/array/arr_2nd.js
@@ -3,12 +3,19 @@
  */
 
 console.log(`welcome to arr_2nd.js`);
-let ele = ["TVS", "BAJAP", "HERO", "U&U", 34, 23, "III"];
+let ele = ["TVS", "BAJAP", "HERO", "U&U", "III"];
 /**
  * slice()
  * its Sliceout from an array & retrun a new Array
  */
 let result = ele.slice(3, -1);
+
+console.log(ele.reverse().join());
+
+result = Array.isArray(ele); // find passed argument was an array or not.
+let finIn = ["error", "One", "Seven", "warning", "One", "log", "Math"];
+
+result = finIn.indexOf("One", 0);
 
 console.log(`================================================================`);
 console.log(`bikes: ${[...ele]}`);
