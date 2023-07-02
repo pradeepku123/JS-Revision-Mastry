0122527ee9b04b8e763c91e05305108d3f5d072a
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-07-02 11:37:14 +0530

    JS

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
  */
 // import "./src/kick_start/missle/range_func.js";
 // import "./src/kick_start/missle/string_word_reverse.js";
-import "./src/kick_start/array/arr_2nd.js";
+// import "./src/kick_start/array/arr_2nd.js";
+import "./src/kick_start/array/arr_3rd.js";
 console.log(`Welcome to index.js`);
---
+++ src/kick_start/array/arr_3rd.js
@@ -0,0 +1,11 @@
+/**
+ * Explre Array Method.
+ */
+console.log(`Welcome to arr3.js`);
+
+let arr = ["work", "ECE", "IT", "CSE", "Work"];
+
+console.log(arr);
+arr.copyWithin(2, 1, 4);
+
+console.log(arr);
